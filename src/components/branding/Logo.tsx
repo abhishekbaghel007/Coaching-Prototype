2b6db1b22d80b649115a27276ba742023337fd64
abhishekbaghel007 <ab9457026733@gmail.com>
2026-09-04 18:10:31 +0530

    Delete src/components directory

--- src/components/branding/Logo.tsx
+++
@@ -1,3 +0,0 @@
-export default function Logo({ compact = false }: { compact?: boolean }) {
-  return <div className="np-logo"><span className="np-logo-mark">N</span>{!compact && <span className="np-logo-name">NEETPrep</span>}</div>;
-}
